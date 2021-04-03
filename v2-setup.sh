257b1450ea7720980dafca89f3cf48c5d3a36224
aarenstade <aarenstade@gmail.com>
2021-04-03 16:51:30 -0700

    lightdm

--- v2-setup.sh
+++ v2-setup.sh
@@ -1,3 +1,11 @@
+
+# set consoleblank=n in /boot/cmdline.txt
 cd boot/
 sudo rm cmdline.txt
+sudo wget https://raw.githubusercontent.com/Mosaic-Billboards/v2-firmware-setup-files/master/cmdline.txt
+cd ..
+
+# remove cursor
+cd etc/lightdm/
+sudo rm lightdm.conf
 sudo wget 
